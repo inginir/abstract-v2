8a329e202e259617825337d2f490ef2e0561efdb
mocerta <malnaggar@acerta.ai>
2024-03-19 20:01:41 -0400

    fix build issues

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,55 +1,9 @@
-import config from "@/config/config.json";
+import ContactForm from "@/components/ContactForm";
 import { getListPage } from "@/lib/contentParser";
 import PageHeader from "@/partials/PageHeader";
 import SeoMeta from "@/partials/SeoMeta";
 import { RegularPage } from "@/types";
 
-export const ContactForm = () => {
-  const { contact_form_action } = config.params;
-  return (
-    <form action={contact_form_action} method="POST">
-      <div className="mb-6">
-        <label htmlFor="name" className="form-label">
-          Full Name <span className="text-red-500">*</span>
-        </label>
-        <input
-          id="name"
-          name="name"
-          className="form-input"
-          placeholder="John Doe"
-          type="text"
-        />
-      </div>
-      <div className="mb-6">
-        <label htmlFor="email" className="form-label">
-          Working Mail <span className="text-red-500">*</span>
-        </label>
-        <input
-          id="email"
-          name="email"
-          className="form-input"
-          placeholder="john.doe@email.com"
-          type="email"
-        />
-      </div>
-      <div className="mb-6">
-        <label htmlFor="message" className="form-label">
-          Anything else? <span className="text-red-500">*</span>
-        </label>
-        <textarea
-          id="message"
-          name="message"
-          className="form-input"
-          placeholder="Message goes here..."
-          rows={8}
-        ></textarea>
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Submit
-      </button>
-    </form>
-  );
-};
 
 const Contact = async () => {
   const data: RegularPage = getListPage("contact/_index.md");

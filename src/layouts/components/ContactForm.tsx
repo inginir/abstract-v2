397eee323cda5e506f1da32e0aee2813ae76fd55
mocerta <malnaggar@acerta.ai>
2024-05-15 00:32:29 +0300

    add email to form

--- src/layouts/components/ContactForm.tsx
+++ src/layouts/components/ContactForm.tsx
@@ -1,9 +1,49 @@
+"use client";
+
 import config from "@/config/config.json";
+import { useState } from "react";
 
 export const ContactForm = () => {
   const { contact_form_action } = config.params;
+
+  const [services, setServices] = useState({
+    maketing_strategy: false,
+    paid_social_advertisement: false,
+    social_media_management: false,
+    consulting: false,
+  });
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
+    const servicesSelected = Object.entries(services).reduce(
+      (acc, [key, value]) => {
+        if (value) {
+          acc.push(key);
+        }
+        return acc;
+      },
+      [] as string[],
+    );
+    data.services = servicesSelected.join(", ");
+
+    try {
+      await fetch("/api/contact", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } catch (error) {
+      console.log("Error", error);
+    }
+  };
+
   return (
-    <form action={contact_form_action} method="POST">
+    <form action={contact_form_action} onSubmit={handleSubmit} method="POST">
       <div className="mb-6">
         <label htmlFor="name" className="form-label">
           Full Name <span className="text-red-500">*</span>
@@ -38,6 +78,9 @@ export const ContactForm = () => {
             type="checkbox"
             value=""
             className="w-4 h-4 text-darkmode-lightbody bg-gray-100 border-gray-300 focus:ring-light dark:focus:ring-light dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            onChange={(e) =>
+              setServices({ ...services, maketing_strategy: e.target.checked })
+            }
           />
           <label className="ms-2 text-md font-medium dark:text-light">
             Maketing Strategy
@@ -49,6 +92,12 @@ export const ContactForm = () => {
             type="checkbox"
             value=""
             className="w-4 h-4 text-darkmode-lightbody bg-gray-100 border-gray-300 focus:ring-light dark:focus:ring-light dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            onChange={(e) =>
+              setServices({
+                ...services,
+                paid_social_advertisement: e.target.checked,
+              })
+            }
           />
           <label className="ms-2 text-md font-medium dark:text-light">
             Paid Social Advertisement
@@ -60,6 +109,12 @@ export const ContactForm = () => {
             type="checkbox"
             value=""
             className="w-4 h-4 text-darkmode-lightbody bg-gray-100 border-gray-300 focus:ring-light dark:focus:ring-light dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            onChange={(e) =>
+              setServices({
+                ...services,
+                social_media_management: e.target.checked,
+              })
+            }
           />
           <label className="ms-2 text-md font-medium dark:text-light">
             Social Media Management
@@ -71,6 +126,9 @@ export const ContactForm = () => {
             type="checkbox"
             value=""
             className="w-4 h-4 text-darkmode-lightbody bg-gray-100 border-gray-300 focus:ring-light dark:focus:ring-light dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            onChange={(e) =>
+              setServices({ ...services, consulting: e.target.checked })
+            }
           />
           <label className="ms-2 text-md font-medium dark:text-light">
             Consulting
---
+++ src/pages/api/contact.ts
@@ -0,0 +1,63 @@
+import sendgrid from "@sendgrid/mail";
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+
+const DEFAULT_SENDER_EMAIL = "moxnaggar@gmail.com";
+
+export default async function contactHandler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === "GET") {
+    res.status(405).json({ error: "Method not allowed" });
+    return;
+  }
+
+  console.log("Subscribing user ...");
+
+  const parsed = req.body;
+
+  const { email } = parsed;
+  console.log(
+    "Initiating subscribe user for email: ",
+    email && `${email.substring(0, 3)}*****@****.***`,
+  );
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ error: "Invalid email format." });
+  }
+
+  try {
+    const data = {
+      from: DEFAULT_SENDER_EMAIL,
+      to: "≈@gmail.com",
+      subject: "Contact us form submission",
+      html: `<div>name: ${parsed?.name}</div>
+                         <div>email: ${parsed?.email}</div>
+                         <div>services selected: ${parsed?.services}</div>
+                         <div>anything else: ${parsed?.message}</div>`,
+    };
+
+    sendgrid.setApiKey(process.env.SENDGRID_API_KEY || "");
+
+    try {
+      const sentInfo = await sendgrid.send(data);
+      console.log(
+        "Email sent successfully",
+        sentInfo,
+      );
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: "Internal server error" });
+    }
+
+    return res.status(200).json({ message: "Email sent successfully" });
+  } catch (error: any) {
+    console.error(error);
+    return new Response("Internal server error", {
+      status: 500,
+    });
+  }
+}
